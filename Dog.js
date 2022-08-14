7407b163143dfe3e8701aed65a9f0f1f13716e30
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2022-08-14 18:35:26 +0100

    finished js interactions

--- Dog.js
+++ Dog.js
@@ -1,1 +1,43 @@
 // Create the Dog class here
+class Dog {
+    constructor(data) {
+        //assign data from data.js to this
+        Object.assign(this, data);
+    }
+    setDogDisplay() {
+        const { name, age, bio, avatar } = this;
+        return `
+                <div class='fade-in'>
+                    <img
+                        src="${avatar}"
+                        alt="Profile Picture"
+                        class="avatar"
+                    />
+
+                    <div id="badge">
+                    </div>
+
+                    <div class="profile-info">
+                        <h1>${name}, ${age}</h1>
+                        <p>${bio}</p>
+                    </div>
+                </div>
+                
+        `;
+    }
+
+    setActionDisplay() {
+        const { hasBeenSwiped, hasBeenLiked } = this;
+        let action;
+
+        hasBeenLiked && hasBeenSwiped
+            ? (action = `<img src="images/badge-like.png" alt="like badge" class=""/>`)
+            : !hasBeenLiked && hasBeenSwiped
+            ? (action = `<img src="images/badge-nope.png" alt="nope badge" class=""/>`)
+            : (action = '');
+
+        return action;
+    }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Remember to import the data and Dog class!
+import dogs from './data.js';
+import Dog from './Dog.js';
+
+const getDogObject = () => {
+    let currentDog = dogs.pop();
+    return currentDog ? new Dog(currentDog) : null;
+};
+
+let dog = getDogObject();
+
+const render = () => {
+    if (!dog) {
+        document.querySelector('main').innerHTML = `
+                                                    <p class='notify'>
+                                                        No more dogs are available at this time. Try again next time!
+                                                    </p>
+                                                    <img
+                                                        src="images/logo.png"
+                                                        alt="Tindog logo"
+                                                        class="header-icon logo"
+                                                    />
+                                                `;
+    } else {
+        document.getElementById('profile').innerHTML = dog.setDogDisplay();
+    }
+};
+
+render();
+
+const renderActions = () => {
+    dog.hasBeenSwiped = true;
+    document.getElementById('badge').innerHTML = dog.setActionDisplay();
+
+    if (dog.hasBeenSwiped) {
+        setTimeout(() => {
+            dog = getDogObject();
+            render();
+        }, 1250);
+    }
+};
+
+document.getElementById('nope-btn').addEventListener('click', () => {
+    dog.hasBeenLiked = false;
+    renderActions();
+});
+
+document.getElementById('like-btn').addEventListener('click', () => {
+    dog.hasBeenLiked = true;
+    renderActions();
+});
